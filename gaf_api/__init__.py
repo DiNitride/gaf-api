cdf2d8cfbdb64d2f606e40a1eb3f1dd6cd64f84e
DiNitride <j.ptb.t.ca@gmail.com>
2017-10-20 22:40:17 +0100

    FUCK JOSE

--- gaf_api/__init__.py
+++ gaf_api/__init__.py
@@ -1,7 +1,7 @@
 from pyramid.config import Configurator
 from pyramid.renderers import json_renderer_factory
 from pyramid.authorization import ACLAuthorizationPolicy
-from gaf_api.auth.oauth import BearerAuthenticationPolicy, JwtHelper, groups
+# from gaf_api.auth.oauth import BearerAuthenticationPolicy, JwtHelper, groups
 from gaf_api.resources import Root
 from os import getenv
 import logging
@@ -17,9 +17,9 @@ def main(global_config, **settings):
     config.set_root_factory(get_root)
     config.add_renderer(None, json_renderer_factory)
 
-    helper = JwtHelper(key=getenv("JWT_KEY"))
-    config.set_authentication_policy(BearerAuthenticationPolicy(groups, jwt_helper=helper))
-    config.set_authorization_policy(ACLAuthorizationPolicy())
+    # helper = JwtHelper(key=getenv("JWT_KEY"))
+    # config.set_authentication_policy(BearerAuthenticationPolicy(groups, jwt_helper=helper))
+    # config.set_authorization_policy(ACLAuthorizationPolicy())
 
     # API v1
     config.add_route('v1:calendar/events', '/api/v1/calendar/events')
@@ -31,6 +31,7 @@ def main(global_config, **settings):
     # OAuth
     config.add_route('oauth:authenticate', '/oauth2/authenticate')
     config.add_route('oauth:authorize', '/oauth2/authorize')
+    config.add_route("oauth:@me", "/oauth2/@me")
     config.scan(".auth_views")
 
     return config.make_wsgi_app()
